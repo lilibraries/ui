61bcdd8fa8fbc129eb9e6ac918165475ad18a7ab
李蔚生 <971353677@qq.com>
2022-07-02 10:42:58 +0800

    feat: add Size

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,9 @@
 export * from "./Icon";
 export * from "./Prefix";
+export * from "./Size";
 export * from "./Transition";
 
 export { default as Icon } from "./Icon";
 export { default as Prefix } from "./Prefix";
+export { default as Size } from "./Size";
 export { default as Transition } from "./Transition";
