bcb31140945d1243373a51265a10f92fb726fc36
李蔚生 <971353677@qq.com>
2022-07-02 11:19:09 +0800

    feat: add Theme

---
+++ packages/ui/src/Theme/index.tsx
@@ -0,0 +1,75 @@
+import React, {
+  FC,
+  ReactNode,
+  cloneElement,
+  ReactElement,
+  isValidElement,
+} from "react";
+import cn from "classnames";
+import { isBrowser, useIsomorphicLayoutEffect } from "@lilib/hooks";
+import Prefix from "../Prefix";
+
+export type ThemeValue = null | "light" | "dark";
+export interface ThemeScopedProps {
+  scoped: true;
+  value?: ThemeValue;
+  children: ReactElement;
+}
+export interface ThemeUnscopedProps {
+  scoped?: false;
+  value?: ThemeValue;
+  children?: ReactNode;
+}
+
+const Theme: FC<ThemeScopedProps | ThemeUnscopedProps> = (props) => {
+  const { value, scoped, children, ...rest } = props;
+  const prefix = Prefix.useConfig();
+
+  useIsomorphicLayoutEffect(() => {
+    if (isBrowser && !scoped) {
+      const lightClassName = `${prefix}light`;
+      const darkClassName = `${prefix}dark`;
+      const classList = document.documentElement.classList;
+
+      if (value === "light") {
+        if (classList.contains(darkClassName)) {
+          classList.remove(darkClassName);
+        }
+        if (!classList.contains(lightClassName)) {
+          classList.add(lightClassName);
+        }
+      } else if (value === "dark") {
+        if (classList.contains(lightClassName)) {
+          classList.remove(lightClassName);
+        }
+        if (!classList.contains(darkClassName)) {
+          classList.add(darkClassName);
+        }
+      } else {
+        if (classList.contains(lightClassName)) {
+          classList.remove(lightClassName);
+        }
+        if (classList.contains(darkClassName)) {
+          classList.remove(darkClassName);
+        }
+      }
+    }
+  }, [scoped, value, prefix]);
+
+  if (isValidElement(children)) {
+    return cloneElement(children, {
+      ...rest,
+      ...children.props,
+      className: cn(
+        (rest as any).className,
+        { [`${prefix}${value}`]: scoped && value },
+        children.props.className
+      ),
+      style: Object.assign({}, (rest as any).style, children.props.style),
+    });
+  } else {
+    return <>{children}</>;
+  }
+};
+
+export default Theme;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,10 +2,12 @@ export * from "./Duration";
 export * from "./Icon";
 export * from "./Prefix";
 export * from "./Size";
+export * from "./Theme";
 export * from "./Transition";
 
 export { default as Duration } from "./Duration";
 export { default as Icon } from "./Icon";
 export { default as Prefix } from "./Prefix";
 export { default as Size } from "./Size";
+export { default as Theme } from "./Theme";
 export { default as Transition } from "./Transition";
