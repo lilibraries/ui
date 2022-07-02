6278711bf8f0eb4e4c0d32b65dddcdf90783a5f0
李蔚生 <971353677@qq.com>
2022-07-02 10:34:46 +0800

    feat: add Transition

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./Icon";
 export * from "./Prefix";
+export * from "./Transition";
 
 export { default as Icon } from "./Icon";
 export { default as Prefix } from "./Prefix";
+export { default as Transition } from "./Transition";
