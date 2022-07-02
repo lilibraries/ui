e91b914044395b288b3f42b9bff07ec82f6e3ee2
李蔚生 <971353677@qq.com>
2022-07-02 10:46:59 +0800

    feat: add Duration

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,8 +1,10 @@
+export * from "./Duration";
 export * from "./Icon";
 export * from "./Prefix";
 export * from "./Size";
 export * from "./Transition";
 
+export { default as Duration } from "./Duration";
 export { default as Icon } from "./Icon";
 export { default as Prefix } from "./Prefix";
 export { default as Size } from "./Size";
