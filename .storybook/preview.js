17e7ab7cdd2fe1e3f59614473ea5fa748a34b357
李蔚生 <971353677@qq.com>
2022-12-26 23:53:22 +0800

    chore: add color palettes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { useDarkMode } from "storybook-dark-mode";
 import { useLayoutMount } from "@lilib/hooks";
-import { Theme, Size, Compact } from "@lilib/ui";
+import { Theme, Size } from "@lilib/ui";
 import { DocsContainer as BaseContainer } from "@storybook/addon-docs/blocks";
 import order from "./order";
 import { light, dark } from "./themes";
@@ -12,32 +12,30 @@ export const parameters = {
   docs: {
     container: function DocsContainer({ children, context }) {
       const isDarkMode = useDarkMode();
-      const { size, compact = null } = context.globals;
+      const { size } = context.globals;
       return (
         <Theme value={isDarkMode ? "dark" : "light"}>
           <Size value={size}>
-            <Compact value={compact}>
-              <BaseContainer
-                context={{
-                  ...context,
-                  storyById: (id) => {
-                    const storyContext = context.storyById(id);
-                    return {
-                      ...storyContext,
-                      parameters: {
-                        ...storyContext?.parameters,
-                        docs: {
-                          ...storyContext?.parameters?.docs,
-                          theme: isDarkMode ? dark : light,
-                        },
+            <BaseContainer
+              context={{
+                ...context,
+                storyById: (id) => {
+                  const storyContext = context.storyById(id);
+                  return {
+                    ...storyContext,
+                    parameters: {
+                      ...storyContext?.parameters,
+                      docs: {
+                        ...storyContext?.parameters?.docs,
+                        theme: isDarkMode ? dark : light,
                       },
-                    };
-                  },
-                }}
-              >
-                {children}
-              </BaseContainer>
-            </Compact>
+                    },
+                  };
+                },
+              }}
+            >
+              {children}
+            </BaseContainer>
           </Size>
         </Theme>
       );
@@ -53,22 +51,11 @@ export const globalTypes = {
       items: [{ value: null, title: "null" }, "small", "large"],
     },
   },
-  compact: {
-    name: "Compact",
-    toolbar: {
-      showName: true,
-      items: [
-        { value: null, title: "null" },
-        { value: true, title: "true" },
-        { value: false, title: "false" },
-      ],
-    },
-  },
 };
 
 export const decorators = [
   (render, context) => {
-    const { size, compact = null } = context.globals;
+    const { size } = context.globals;
     const demoContainerRef = useRef();
 
     useLayoutMount(() => {
@@ -83,9 +70,7 @@ export const decorators = [
     return (
       <Theme value={useDarkMode() ? "dark" : "light"}>
         <Size value={size}>
-          <Compact value={compact}>
-            <div ref={demoContainerRef}>{render()}</div>
-          </Compact>
+          <div ref={demoContainerRef}>{render()}</div>
         </Size>
       </Theme>
     );
