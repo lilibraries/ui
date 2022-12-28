96b18fa5b9c6f6820d284fd9c27d675c2f08a8a7
李蔚生 <971353677@qq.com>
2022-12-28 20:29:41 +0800

    chore: finish `Size` component

--- packages/ui/src/Size/index.ts
+++ packages/ui/src/Size/index.ts
@@ -1,8 +1,11 @@
+import { ReactNode } from "react";
 import createConfig from "../utils/createConfig";
 
 export type SizeValue = null | "small" | "large";
+
 export interface SizeProps {
   value?: SizeValue;
+  children?: ReactNode;
 }
 
 const Size = createConfig<SizeValue, SizeProps>("value", null);
---
+++ stories/Size/Size.stories.tsx
@@ -0,0 +1,6 @@
+export default {
+  title: "Examples/Size",
+};
+
+export { default as Basic } from "./Basic";
+export { default as Nested } from "./Nested";
