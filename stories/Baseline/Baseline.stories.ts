5fcd00ecc0cb9a059d3a2fe076decf7489ba8979
李蔚生 <971353677@qq.com>
2022-12-29 23:51:33 +0800

    chore(Baseline): add some reset styles
    Includes: inline elements, horizontal rule, preformatted text, blockquote and lists.

--- stories/Baseline/Baseline.stories.ts
+++ stories/Baseline/Baseline.stories.ts
@@ -4,4 +4,9 @@ export default {
 
 export { default as Headings } from "./Headings";
 export { default as Paragraphs } from "./Paragraphs";
+export { default as InlineElements } from "./InlineElements";
 export { default as HorizontalRule } from "./HorizontalRule";
+export { default as PreformattedText } from "./PreformattedText";
+export { default as Blockquote } from "./Blockquote";
+export { default as Lists } from "./Lists";
+export { default as Table } from "./Table";
---
+++ stories/Baseline/Table.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Table() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Table heading</th>
+          <th>Table heading</th>
+          <th>Table heading</th>
+          <th>Table heading</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+        </tr>
+        <tr>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+        </tr>
+        <tr>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+        </tr>
+      </tbody>
+      <caption>
+        This is an example table, and this is its caption to describe the
+        contents.
+      </caption>
+    </table>
+  );
+}
+
+export default Table;
