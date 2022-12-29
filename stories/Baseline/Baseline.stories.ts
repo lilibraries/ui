3a903bde6bb3f6122baffe11fd322843f9abf1a4
李蔚生 <971353677@qq.com>
2022-12-29 19:08:45 +0800

    chore: move normalize.css into baseline own style file

--- stories/Baseline/Baseline.stories.ts
+++ stories/Baseline/Baseline.stories.ts
@@ -3,5 +3,5 @@ export default {
 };
 
 export { default as Headings } from "./Headings";
-export { default as Paragraph } from "./Paragraph";
+export { default as Paragraphs } from "./Paragraphs";
 export { default as HorizontalRule } from "./HorizontalRule";
