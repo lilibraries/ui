0417472e8e3d30bb5b780424caedd0dbee22d05d
李蔚生 <971353677@qq.com>
2022-12-31 23:23:22 +0800

    chore: finish `Dot` component

--- stories/Dot/Sizes.tsx
+++ stories/Dot/Sizes.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Dot, DotProps } from "@lilib/ui";
+import { Flexbox, Dot, DotProps } from "@lilib/ui";
 
 function Sizes(props: DotProps) {
   return (
-    <>
-      <Dot {...props} size="small" /> <Dot {...props} size={null} />{" "}
+    <Flexbox gap="2x" align="center">
+      <Dot {...props} size="small" />
+      <Dot {...props} size={null} />
       <Dot {...props} size="large" />
-    </>
+    </Flexbox>
   );
 }
 
