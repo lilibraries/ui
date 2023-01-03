733310ffd38668c7395bf04c5ddbbc63339fa9f9
李蔚生 <971353677@qq.com>
2023-01-03 22:41:14 +0800

    chore: remove border-width and font-weight variables

--- stories/Switch/Labelled.tsx
+++ stories/Switch/Labelled.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Switch, SwitchProps } from "@lilib/ui";
-import { FiCheck, FiSettings, FiX } from "react-icons/fi";
+import { Flexbox, Switch, SwitchProps } from "@lilib/ui";
+import { FiCheck, FiX } from "react-icons/fi";
 
 function Labelled(props: SwitchProps) {
   return (
-    <>
-      <Switch {...props} icon={<FiSettings />} />{" "}
-      <Switch {...props} checkedLabel="🌛" uncheckedLabel="☀️" />{" "}
-      <Switch {...props} checkedLabel={<FiCheck />} uncheckedLabel={<FiX />} />{" "}
+    <Flexbox gap="2x" align="center">
+      <Switch {...props} checkedLabel="🌛" uncheckedLabel="☀️" />
+      <Switch {...props} checkedLabel={<FiCheck />} uncheckedLabel={<FiX />} />
       <Switch {...props} checkedLabel="On" uncheckedLabel="Off" />
-    </>
+    </Flexbox>
   );
 }
 
--- stories/Switch/Switch.stories.tsx
+++ stories/Switch/Switch.stories.tsx
@@ -17,6 +17,12 @@ export default {
     loading: {
       control: { type: "boolean" },
     },
+    loadingIcon: {
+      control: { type: "text" },
+    },
+    loadingDelay: {
+      control: { type: "number" },
+    },
     disabled: {
       control: { type: "boolean" },
     },
@@ -27,6 +33,8 @@ export default {
 };
 
 export { default as Basic } from "./Basic";
+export { default as Icon } from "./Icon";
 export { default as Labelled } from "./Labelled";
 export { default as Sizes } from "./Sizes";
 export { default as Loading } from "./Loading";
+export { default as Disabled } from "./Disabled";
