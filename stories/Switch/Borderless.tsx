fc98cf8e5fc8ba4c31c1d721d163417048c1570e
李蔚生 <971353677@qq.com>
2023-01-10 19:33:09 +0800

    chore: improve Switch component

--- stories/Switch/Borderless.tsx
+++ stories/Switch/Borderless.tsx
@@ -3,15 +3,34 @@ import { Flexbox, Switch, SwitchProps } from "@lilib/ui";
 
 function Borderless(props: SwitchProps) {
   return (
-    <Flexbox gap="2x" align="center">
-      <Switch checkedLabel="On" uncheckedLabel="Off" {...props} borderless />
-      <Switch
-        defaultChecked
-        checkedLabel="On"
-        uncheckedLabel="Off"
-        {...props}
-        borderless
-      />
+    <Flexbox direction="column" gap="4x" align="flex-start">
+      <Flexbox gap="2x" align="center">
+        <Switch checkedLabel="On" uncheckedLabel="Off" {...props} borderless />
+        <Switch
+          defaultChecked
+          checkedLabel="On"
+          uncheckedLabel="Off"
+          {...props}
+          borderless
+        />
+      </Flexbox>
+      <Flexbox gap="2x" align="center">
+        <Switch
+          checkedLabel="On"
+          uncheckedLabel="Off"
+          {...props}
+          disabled
+          borderless
+        />
+        <Switch
+          defaultChecked
+          checkedLabel="On"
+          uncheckedLabel="Off"
+          {...props}
+          disabled
+          borderless
+        />
+      </Flexbox>
     </Flexbox>
   );
 }
