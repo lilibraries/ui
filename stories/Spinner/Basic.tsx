731146c6516d0f2b543680fba7f35847b0c3ad1a
李蔚生 <971353677@qq.com>
2023-01-11 19:45:01 +0800

    chore: improve Spinner stories

--- stories/Spinner/Basic.tsx
+++ stories/Spinner/Basic.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import { Spinner, SpinnerProps, Switch } from "@lilib/ui";
+import { Flexbox, Spinner, SpinnerProps, Switch } from "@lilib/ui";
 
 function Basic(props: SpinnerProps) {
   const [spinning, setSpinning] = useState(true);
 
   return (
-    <>
+    <Flexbox gap="4x" align="center">
       <Switch
         checked={spinning}
         onChange={(event) => setSpinning(event.target.checked)}
-      />{" "}
-      <Spinner {...props} spinning={spinning} endSpace />
-      Spinner
-    </>
+      />
+      <div>
+        <Spinner {...props} spinning={spinning} endSpace />
+        Spinner
+      </div>
+    </Flexbox>
   );
 }
 
--- stories/Spinner/Delay.tsx
+++ stories/Spinner/Delay.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import { Spinner, SpinnerProps, Switch } from "@lilib/ui";
+import { Flexbox, Spinner, SpinnerProps, Switch } from "@lilib/ui";
 
 function Delay(props: SpinnerProps) {
   const [spinning, setSpinning] = useState(false);
 
   return (
-    <>
+    <Flexbox gap="4x" align="center">
       <Switch
         checked={spinning}
         onChange={(event) => setSpinning(event.target.checked)}
-      />{" "}
-      <Spinner {...props} spinning={spinning} delay={500} endSpace />
-      Spinner
-    </>
+      />
+      <div>
+        <Spinner {...props} spinning={spinning} delay={500} endSpace />
+        Spinner
+      </div>
+    </Flexbox>
   );
 }
 
