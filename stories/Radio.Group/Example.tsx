c51664c4d61fb987249f18be2b3ce2ca3e068e75
李蔚生 <971353677@qq.com>
2023-02-01 08:09:39 +0800

    chore: merge Radio and Radio.Group docs

--- stories/Radio.Group/Example.tsx
+++ stories/Radio.Group/Example.tsx
@@ -21,4 +21,28 @@ function Controlled(props: RadioGroupProps) {
   );
 }
 
-export default Controlled;
+function Uncontrolled(props: RadioGroupProps) {
+  return (
+    <Radio.Group defaultValue="value 1" {...props}>
+      <Flexbox gap="4x">
+        <Radio value="value 1">Option 1</Radio>
+        <Radio value="value 2">Option 2</Radio>
+        <Radio value="value 3">Option 3</Radio>
+      </Flexbox>
+    </Radio.Group>
+  );
+}
+
+function Example(props: RadioGroupProps) {
+  return (
+    <div>
+      <h6>Controlled</h6>
+      <Controlled {...props} />
+
+      <h6>Uncontrolled</h6>
+      <Uncontrolled {...props} />
+    </div>
+  );
+}
+
+export default Example;
