3eff5bd50badc5a71806a99a1606bad3af5d8719
李蔚生 <971353677@qq.com>
2023-03-25 13:04:56 +0800

    chore: finish design tokens

--- stories/Tokens/RGBs.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-import { names } from "./Colors";
-import PaletteRGBs from "../Palettes/RGBs";
-
-const RGBs: FC = () => {
-  return <PaletteRGBs names={names} />;
-};
-
-export default RGBs;
