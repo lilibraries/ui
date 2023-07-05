a9b6a2e5e869a2b7cee27d3058c7ec5206c9d2a9
李蔚生 <971353677@qq.com>
2023-07-05 23:12:54 +0800

    chore: improve Image component

--- stories/Image/Basic.tsx
+++ stories/Image/Basic.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Image, ImageProps } from "@lilib/ui";
+import { Image } from "@lilib/ui";
 
-function Basic(props: ImageProps) {
+function Example() {
   return (
     <Image
-      {...props}
-      alt="Landscape"
-      style={{ width: 200, height: 113, objectFit: "cover" }}
-      src="https://images.unsplash.com/photo-1547534601-862c877b1923?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDExOXw2c01WalRMU2tlUXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=200&q=60"
+      alt="Image"
+      src="https://images.unsplash.com/photo-1682687218608-5e2522b04673"
+      style={{ width: 200, height: 200, objectFit: "cover" }}
     />
   );
 }
 
-export default Basic;
+export default Example;
