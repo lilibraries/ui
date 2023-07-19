3cdacd05928d2845e09ce7a2213200e9a457a9a0
李蔚生 <971353677@qq.com>
2023-07-19 21:25:43 +0800

    feat: add Info component

---
+++ stories/Info/Indicators.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Avatar,
+  Checkbox,
+  Flexbox,
+  Info,
+  Radio,
+  Spinner,
+  Switch,
+} from "@lilib/ui";
+import { FiInfo } from "react-icons/fi";
+
+function Example() {
+  return (
+    <Flexbox direction="column" gap="8x" fluid>
+      <Info indicator={<FiInfo />}>
+        <Info.Title>Icon</Info.Title>
+        <Info.Detail>This is a descriptive message.</Info.Detail>
+      </Info>
+
+      <Info
+        indicator={
+          <img
+            alt="Avatar"
+            src="https://images.unsplash.com/photo-1684837955373-dd25d9e1c3b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE5OHxKcGc2S2lkbC1Ia3x8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+          />
+        }
+      >
+        <Info.Title>Image</Info.Title>
+        <Info.Detail>This is a descriptive message.</Info.Detail>
+      </Info>
+
+      <Info indicator={<Spinner spinning />}>
+        <Info.Title>Spinner</Info.Title>
+        <Info.Detail>This is a descriptive message.</Info.Detail>
+      </Info>
+
+      <Info indicator={<Radio />}>
+        <Info.Title>Radio</Info.Title>
+        <Info.Detail>This is a descriptive message.</Info.Detail>
+      </Info>
+
+      <Info indicator={<Checkbox />}>
+        <Info.Title>Checkbox</Info.Title>
+        <Info.Detail>This is a descriptive message.</Info.Detail>
+      </Info>
+
+      <Info indicator={<Switch size="small" />}>
+        <Info.Title>Switch</Info.Title>
+        <Info.Detail>This is a descriptive message.</Info.Detail>
+      </Info>
+
+      <Info
+        indicator={
+          <Avatar
+            size="small"
+            image="https://images.unsplash.com/photo-1684837955373-dd25d9e1c3b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE5OHxKcGc2S2lkbC1Ia3x8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+          />
+        }
+      >
+        <Info.Title>Avatar</Info.Title>
+        <Info.Detail>This is a descriptive message.</Info.Detail>
+      </Info>
+    </Flexbox>
+  );
+}
+
+export default Example;
---
+++ stories/Info/Padding.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Info } from "@lilib/ui";
+
+function Example() {
+  return (
+    <Info padding>
+      <Info.Title>What is React?</Info.Title>
+      <Info.Detail>
+        React (also known as React.js or ReactJS) is a free and open-source
+        front-end JavaScript library for building user interfaces or UI
+        components. It is maintained by Facebook and a community of individual
+        developers and companies.
+      </Info.Detail>
+    </Info>
+  );
+}
+
+export default Example;
