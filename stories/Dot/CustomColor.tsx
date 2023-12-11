7b697a015f8012816484d3634dfb1ae79b151643
李蔚生 <971353677@qq.com>
2023-12-11 23:06:45 +0800

    chore: finish Dot component

--- stories/Dot/CustomColor.tsx
+++ stories/Dot/CustomColor.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Dot } from "@lilib/ui";
 
 function Example() {
-  return <Dot color="#de2910">Dot</Dot>;
+  return (
+    <>
+      <Dot color="#de2910" /> Dot
+    </>
+  );
 }
 
 export default Example;
