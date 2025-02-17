e514d59ac1f786c192423282b3c86a896e41fb7d
李蔚生 <971353677@qq.com>
2025-02-17 23:51:39 +0800

    chore: improve Root component

--- packages/ui/src/Root/index.tsx
+++ packages/ui/src/Root/index.tsx
@@ -1,11 +1,11 @@
-import React, { FC, ReactNode } from "react";
+import { FC, ReactNode } from "react";
 
 export interface RootProps {
   children?: ReactNode;
 }
 
 const Root: FC<RootProps> = ({ children }) => {
-  return <>{children}</>;
+  return children;
 };
 
 export default Root;
